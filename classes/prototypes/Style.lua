36d09785c974be71c078c82b90495d205874851e
RitnDev <ritn@hotmail.fr>
2023-02-10 14:43:18 +0100

    0.7.5 - prepare future version 0.7.5 (2)

--- classes/prototypes/Style.lua
+++ classes/prototypes/Style.lua
@@ -7,15 +7,21 @@ local RitnProtoBase = require("__RitnLib__.classes.RitnClass.RitnPrototype")
 
 
 ----------------------------------------------------------------
-local RitnProtoStyle = class.newclass(RitnProtoBase, function(base)
+local RitnProtoStyle = class.newclass(RitnProtoBase, function(base, style_name)
     -- prototype init
     RitnProtoBase.init(base, "default", "gui-style")
     --------------------------------------------------
     -- prototype base
     base.object_name = "RitnProtoStyle"
+    base.style_name = style_name
     ----
     if data.raw[base.type][base.name] == nil then return end
-    base.prototype = table.deepcopy(data.raw[base.type][base.name])
+    if base.style_name ~= nil then
+        if data.raw[base.type][base.name][base.style_name] == nil then return end
+        base.prototype = table.deepcopy(data.raw[base.type][base.name][base.style_name])
+    else
+        base.prototype = table.deepcopy(data.raw[base.type][base.name])
+    end
     ----
     base.style = {
         type = "",
@@ -89,7 +95,11 @@ end
 function RitnProtoStyle:update()
     if data.raw[self.type][self.name] == nil then return self end
     if self.prototype == nil then return self end
-    data.raw[self.type][self.name] = self.prototype
+    if self.style_name ~= nil then 
+        data.raw[self.type][self.name][self.style_name] = self.prototype
+    else
+        data.raw[self.type][self.name] = self.prototype
+    end
 end
 
 
